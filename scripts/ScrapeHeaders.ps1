19d80e5a6926fdd8a668b13f7206aa64c7f38292
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2023-02-01 10:47:53 -0800

    Imported ScrapeHeaders.ps1.

---
+++ scripts/ScrapeHeaders.ps1
@@ -0,0 +1,36 @@
+param
+(
+    [switch]$Clean,
+
+    [string]$Partition,
+
+    [switch]$Debug
+)
+
+. "$PSScriptRoot\CommonUtils.ps1"
+
+Install-BuildTools
+
+if ($Clean.IsPresent)
+{
+    .\scripts\CleanOutputs.ps1
+}
+
+if ($Debug)
+{
+    $configuration = "Debug"
+}
+else
+{
+    $configuration = "Release"
+}
+
+$params = @('build', $wdkProjectRoot, "-c:$configuration", '-t:ScrapeHeaders')
+if ($Partition)
+{
+    $params += "-p:PartitionFilter=$Partition"
+}
+
+& dotnet $params
+
+ThrowOnNativeProcessError
