2c215df31c04ac29d6ad21216af209dd8627402f
Phil Nachreiner <philnach@microsoft.com>
2024-10-28 16:13:52 -0700

    Add verbose logging

--- scripts/CommonUtils.ps1
+++ scripts/CommonUtils.ps1
@@ -71,7 +71,7 @@ function Install-BuildTools
         & dotnet clean "$rootDir\buildtools"
     }
 
-    & dotnet build "$rootDir\buildtools" -c Release "-bl:$PSScriptRoot\..\bin\logs\buildtools.binlog"
+    & dotnet build "$rootDir\buildtools" -c Release "-bl:$PSScriptRoot\..\bin\logs\buildtools.binlog" --verbosity detailed
     ThrowOnNativeProcessError
 
     Install-VsDevShell
