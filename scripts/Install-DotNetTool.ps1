6b9baf466cc650faf3a00cc546312de8b43374e0
Phil Nachreiner <philnach@microsoft.com>
2024-10-29 21:08:51 -0700

    Tweak the message

--- scripts/Install-DotNetTool.ps1
+++ scripts/Install-DotNetTool.ps1
@@ -1,11 +1,11 @@
 Param ([string] $Name, [string] $Version = '', [string] $NuGetConfigFile)
 
 if (-not $NuGetConfigFile) {
-    throw "NuGetConfigFile is null must be a valid file path to NuGet.config file."
+    throw "NuGetConfigFile is null. NuGetConfigFile must be a valid file path to a NuGet.config file."
 }
 
 if (-not (Test-Path -Path $NuGetConfigFile -PathType Leaf)) {
-	throw "NuGetConfigFile file wasn't found at supplied path valid path to NuGet.config file."
+	throw "NuGetConfigFile file wasn't found at supplied path. NuGetConfigFile must be a valid file path to a NuGet.config file."
 }
 
 if ($Version -ne '')
