1f7d963754d2f881baa775d8b2ba62d7aef704fa
Phil Nachreiner <philnach@microsoft.com>
2024-11-15 16:06:49 -0800

    More logging

--- scripts/Install-DotNetTool.ps1
+++ scripts/Install-DotNetTool.ps1
@@ -8,19 +8,28 @@ if (-not (Test-Path -Path $NuGetConfigFile -PathType Leaf)) {
 	throw "NuGetConfigFile file wasn't found at supplied path. NuGetConfigFile must be a valid file path to a NuGet.config file."
 }
 
+Write-Host "Hello Mom"
+
 if ($Version -ne '')
 {
+    Write-Host "Hello Mom 2"
+
     $installed = & dotnet tool list -g | select-string -Pattern "$Name\s+$Version"
     if ($installed -eq $null)
     {
-        & dotnet tool update --global $Name --version $Version --configfile $NuGetConfigFile
+        Write-Host "Hello Mom 3"
+
+        & dotnet tool update --global $Name --version $Version --configfile $NuGetConfigFile -v diag
     }
 }
 else
 {
+    Write-Host "Hello Mom 4"
+
     $installed = & dotnet tool list -g | select-string -Pattern "$Name"
     if ($installed -eq $null)
     {
-        & dotnet tool update --global $Name --configfile $NuGetConfigFile
+        Write-Host "Hello Mom 5"
+        & dotnet tool update --global $Name --configfile $NuGetConfigFile -v diag
     }
 }
